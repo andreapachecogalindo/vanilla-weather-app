444138a3a2d1573d27cc111fe4239955d77859c8
andreapachecogalindo <67153792+andreapachecogalindo@users.noreply.github.com>
2021-02-05 16:15:55 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.date.name;
+  cityElement.innerHTML = response.data.name;
 }
 
 let apiKey = "f54498ba0c2b84b117a6bad5fe404784";
