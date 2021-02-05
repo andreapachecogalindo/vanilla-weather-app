495663fb47147db53bf9b7e4c6d5edfcad2cb609
andreapachecogalindo <67153792+andreapachecogalindo@users.noreply.github.com>
2021-02-05 15:40:05 +0100

    api Url update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
 let apiKey = "f54498ba0c2b84b117a6bad5fe404784:";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
