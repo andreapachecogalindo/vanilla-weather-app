9e8eecdd25cca7bcd95bdbd4f0dbf1a968ed38ac
andreapachecogalindo <67153792+andreapachecogalindo@users.noreply.github.com>
2021-02-05 16:58:43 +0100

    temp parameters update js

--- src/app.js
+++ src/app.js
@@ -10,10 +10,10 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  feelsElement.innerHTML = response.data.main.feel_like;
+  feelsElement.innerHTML = Math.round(response.data.main.feels_like);
   humidityElement.innerHTML = response.data.main.humidity;
-  maxElement.innerHTML = response.data.main.temp_max;
-  minElement.innerHTML = response.data.main.temp_min;
+  maxElement.innerHTML = Math.round(response.data.main.temp_max);
+  minElement.innerHTML = Math.round(response.data.main.temp_min);
 }
 let apiKey = "f54498ba0c2b84b117a6bad5fe404784";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=${apiKey}&units=metric`;
