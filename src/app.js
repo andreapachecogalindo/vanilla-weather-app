10e45397f101eff474b0cc4aba4e98360466500e
andreapachecogalindo <67153792+andreapachecogalindo@users.noreply.github.com>
2021-02-05 15:37:11 +0100

    weather unit metric

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 let apiKey = "f54498ba0c2b84b117a6bad5fe404784:";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
